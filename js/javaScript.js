53ad729d3f447b7ff25ff7daa5e7a2f35d29141e
Warlte <malte.cassel@gmail.com>
2021-05-28 22:17:28 +0200

    yes
    yes

--- js/javaScript.js
+++ js/javaScript.js
@@ -12,17 +12,36 @@ window.addEventListener('scroll', function(){
     text.style.top = value * 1 + 'px';
 })
 
-document.body.style.backgroundColor = localStorage.getItem("color");
+//laddning av localstorage
+//document.body.style.backgroundColor = localStorage.getItem("jscolorbg");
+//document.documentElement.style.setProperty("--color","jscolor");
+//document.documentElement.style.setProperty("--colortxt","jscolortxt");
+
+
 
 function DarkMode() {
     console.log("klickat Dark")
-    document.body.style.backgroundColor = "black";
-    localStorage.setItem("color", "black");
-    }
+    
+    document.body.style.backgroundColor = "#282828";
+    document.documentElement.style.setProperty("--color","#121212");
+    document.documentElement.style.setProperty("--colortxt","white");
+
+    //sparar färg i localstorage
+    //localStorage.setItem("jscolorbg", "#282828");
+    //localStorage.setItem("jscolor", "#121212");
+    //localStorage.setItem("jscolortxt", "white");
+}    
 
 function LightMode() {
     console.log("klickat Light")
+    
     document.body.style.backgroundColor = "white";
-    localStorage.setItem("color", "white");
-    }
+    document.documentElement.style.setProperty("--color","#e6add8");
+    document.documentElement.style.setProperty("--colortxt","black");
+
+    //sparar färg i localstorage
+    //localStorage.setItem("jscolorbg", "#white");
+    //localStorage.setItem("jscolor", "#e6add8");
+    //localStorage.setItem("jscolortxt", "white");
+}
 
